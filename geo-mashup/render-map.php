e13f3ad69964fef26cd9548199ec4914f371a82f
dylankkuhn <dylankkuhn@81465218-ea22-0410-8f2b-2343bde7f0e1>
2008-12-03 02:43:04 +0000

    Fixed double quote in template url bug. Using require_once based on scanty positive feedback.
    git-svn-id: https://wordpress-geo-mashup.googlecode.com/svn/trunk@227 81465218-ea22-0410-8f2b-2343bde7f0e1

--- geo-mashup/render-map.php
+++ geo-mashup/render-map.php
@@ -1,6 +1,6 @@
 <?php
 
-require ( '../../../wp-blog-header.php' );
+require_once ( '../../../wp-blog-header.php' );
 
 status_header ( 200 );
 geo_mashup_render_map ( );
@@ -18,9 +18,10 @@ function add_double_quotes(&$item,$key) {
 
 function geo_mashup_render_map ( ) {
 	global $post, $geo_mashup_options;
+	$template_url_path = get_bloginfo( 'template_directory' );
 	$map_properties = array ( 
 		'url_path' => GEO_MASHUP_URL_PATH,
- 		'template_url_path' => get_bloginfo('template_directory'));
+ 		'template_url_path' => $template_url_path );
 
 	$map_content = null;
 	if (strlen($_GET['map_content']) > 0) {
@@ -112,7 +113,7 @@ function geo_mashup_render_map ( ) {
 				// find the css file needed
 				if (is_readable(TEMPLATEPATH . '/map-style.css'))
 				{
-					echo '<link rel="stylesheet" type="text/css" href="' . $map_properties['template_url_path'] . '/map-style.css' . '" />';
+					echo '<link rel="stylesheet" type="text/css" href="' . $template_url_path . '/map-style.css' . '" />';
 				}
 				else
 				{
