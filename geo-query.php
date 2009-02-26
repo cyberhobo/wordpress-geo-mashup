3f254ebc0e531a2d92c5f953916c040963e12826
dylankkuhn <dylankkuhn@81465218-ea22-0410-8f2b-2343bde7f0e1>
2009-02-26 01:07:50 +0000

    Use a template to generate info window content. Also some commented code with ideas for better cache handling.
    git-svn-id: https://wordpress-geo-mashup.googlecode.com/svn/trunk/geo-mashup@281 81465218-ea22-0410-8f2b-2343bde7f0e1

--- geo-query.php
+++ geo-query.php
@@ -2,13 +2,10 @@
 
 require_once('../../../wp-blog-header.php');
 
-status_header(200);
-
-$post_id =$_GET['post_id'];
-if (is_numeric($post_id)) { 
-	GeoMashupQuery::query_post($post_id);
+if ( empty( $_GET['post_ids'] ) ) {
+	GeoMashupQuery::generate_location_json( );
 } else {
-	GeoMashupQuery::query_locations();
+	GeoMashupQuery::generate_post_html( $_GET['post_ids'] );
 }
 
 /**
@@ -16,78 +13,43 @@ if (is_numeric($post_id)) {
  */
 class GeoMashupQuery {
 
-	function trim_html($html, $length) {
-		$end_pos = 0;
-		$text_len = 0;
-		$tag_count = 0;
-		while ($text_len<$length) {
-			if ($html[$end_pos] == '<') $tag_count++;
-			else if ($html[$end_pos] == '>') $tag_count--;
-			$end_pos++;
-			if ($tag_count == 0) $text_len++;
+	function generate_post_html($post_ids) {
+		global $geo_mashup_options;
+
+		if ( !is_array( $post_ids ) ) {
+			$post_ids = split( ',', $post_ids );
 		}
-		return substr($html,0,$end_pos);
-	}
 
-	function strip_geo_mashup_shortcodes($content) {
-		return preg_replace('/\[geo_mashup.*?\]/','',$content);
-	}
+		query_posts( array( 'post__in' => $post_ids ) );
 
-	function excerpt_html($content) {
-		global $geo_mashup_options;
-
-		// Geo Mashup shortcodes in excerpts can cause an infinite recursion of frames - remove them
-		$content = GeoMashupQuery::strip_geo_mashup_shortcodes($content);
-		$content = apply_filters('the_content', $content);
-		$content = GeoMashupQuery::trim_html($content,$geo_mashup_options->get('global_map', 'excerpt_length'));
-		$content = balanceTags($content, true);
-		$content = htmlspecialchars($content);
-		return $content;
-	}
-
-	function excerpt_text($content) {
-		global $geo_mashup_options;
-
-		$content = strip_tags($content);
-		$content = substr($content,0,$geo_mashup_options->get('global_map', 'excerpt_length'));
-		$content = htmlspecialchars($content);
-		return $content;
-	}
-
-	function query_post($post_id) {
-		global $geo_mashup_options;
-
-		header('Content-type: text/xml; charset='.get_settings('blog_charset'), true);
-		header('Cache-Control: no-cache;', true);
-		header('Expires: -1;', true);
-
-		echo '<?xml version="1.0" encoding="'.get_settings('blog_charset').'"?'.'>'."\n";
-
-		echo '<channel><title>GeoMashup Query</title><item>';
-		$post = get_post( $post_id );
-		if (!$post) {
-			echo '<title>Post'.$post_id.'not found</title>';
-		} else {
-			$categories = get_the_category( $post_id );
-			foreach ($categories as $category) {
-				echo '<category>'.$category->name.'</category>';
-			}
-			$author = get_author_name( $post->post_author );
-			if ($geo_mashup_options->get('global_map', 'excerpt_format')=='html') {
-				$excerpt = GeoMashupQuery::excerpt_html($post->post_content);
-			} else {
-				$excerpt = GeoMashupQuery::excerpt_text($post->post_content);
-			}
-			echo '<author>'.htmlspecialchars($author).'</author>'.
-				'<pubDate>'.$post->post_date.'</pubDate>'.
-				'<title>'.htmlspecialchars($post->post_title).'</title>'.
-				'<link>'.get_permalink($post_id).'</link>'.
-				'<description>'.$excerpt.'</description>';
+		$template = locate_template( array('geo-mashup-info-window.php') );
+		if ( empty( $template ) ) {
+			$template = 'info-window.php';
 		}
-		echo '</item></channel>';
+		if ( !is_readable( $template ) ) {
+			$template = 'info-window-default.php';
+		}
+		load_template( $template );
 	}
 
-	function query_locations() {
+	function generate_location_json( ) {
+		/*
+		if ( !empty( $_SERVER['HTTP_IF_MODIFIED_SINCE'] ) ) {
+			$http_time = strtotime( $_SERVER['HTTP_IF_MODIFIED_SINCE'] );
+			$mod_time = strtotime( $post->post_modified_gmt . ' GMT' );
+			if ($mod_time <= $http_time) {
+				return status_header(304); // Not modified
+			}
+		}
+
+		status_header(200);
+		header( 'Last-Modified: ' . mysql2date( 'D, d M Y H:i:s', $post->post_modified_gmt, false ) . ' GMT' );
+		header( 'Content-type: text/xml; charset='.get_settings('blog_charset'), true);
+		header( 'Cache-control: max-age=300, must-revalidate', true);
+		header( 'Expires: ' . gmdate( 'D, d M Y H:i:s', time() + 300 ) . " GMT" );
+		header( 'Pragma:' );
+		 */
+		status_header(200);
 		header('Content-type: text/plain; charset='.get_settings('blog_charset'), true);
 		header('Cache-Control: no-cache;', true);
 		header('Expires: -1;', true);
