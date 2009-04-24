5fa59229eff8cbbd930d0e0dcf2008029785e11d
dylankkuhn <dylankkuhn@81465218-ea22-0410-8f2b-2343bde7f0e1>
2009-04-24 04:45:42 +0000

    Added comment location collection using google loader API.
    git-svn-id: https://wordpress-geo-mashup.googlecode.com/svn/trunk@438 81465218-ea22-0410-8f2b-2343bde7f0e1

--- geo-mashup/geo-mashup-loader.js
+++ geo-mashup/geo-mashup-loader.js
@@ -1,4 +1,30 @@
-var GeoMashupLoader = {
+/*global google, GeoMashupLoader */
+var GeoMashupLoader;
+
+/**
+ * Set client location in comment form when appropriate.
+ */
+google.setOnLoadCallback( function() {
+	var lat_element;
+	if ( google.loader.ClientLocation ) {
+		lat_element = document.getElementById( 'geo_mashup_lat_input' );
+		if (lat_element) {
+			lat_element.value = google.loader.ClientLocation.latitude;
+			document.getElementById( 'geo_mashup_lng_input' ).value = google.loader.ClientLocation.longitude;
+			document.getElementById( 'geo_mashup_address_input' ).value = google.loader.ClientLocation.address.city +
+				', ' + google.loader.ClientLocation.address.region + ', ' + google.loader.ClientLocation.address.country_code;
+			document.getElementById( 'geo_mashup_country_code_input' ).value = google.loader.ClientLocation.address.country_code;
+			document.getElementById( 'geo_mashup_locality_input' ).value = google.loader.ClientLocation.address.city;
+		}
+	}
+} );
+
+/**
+ * Geo Mashup Loader object.
+ *
+ * Currently implements click to load feature.
+ */
+GeoMashupLoader = {
 	addMapFrame : function (element, frame_url, height, width, name) {
 		var html = ['<iframe name="'];
 		element.style.backgroundImage = 'none';
