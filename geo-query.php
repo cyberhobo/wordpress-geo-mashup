fd88a7116b94a69f9875e97c2496d93fbfb1e897
dylankkuhn <dylankkuhn@81465218-ea22-0410-8f2b-2343bde7f0e1>
2009-06-21 04:34:18 +0000

    Added locate_template for WP 2.6.
    git-svn-id: https://wordpress-geo-mashup.googlecode.com/svn/trunk/geo-mashup@511 81465218-ea22-0410-8f2b-2343bde7f0e1

--- geo-query.php
+++ geo-query.php
@@ -2,6 +2,32 @@
 
 require_once('../../../wp-blog-header.php');
 
+/**
+ * Establish locate_template for WP 2.6.
+ */
+if ( !function_exists( 'locate_template' ) ) {
+	function locate_template($template_names, $load = false) {
+		if (!is_array($template_names))
+			return '';
+
+		$located = '';
+		foreach($template_names as $template_name) {
+			if ( file_exists(STYLESHEETPATH . '/' . $template_name)) {
+				$located = STYLESHEETPATH . '/' . $template_name;
+				break;
+			} else if ( file_exists(TEMPLATEPATH . '/' . $template_name) ) {
+				$located = TEMPLATEPATH . '/' . $template_name;
+				break;
+			}
+		}
+
+		if ($load && '' != $located)
+			load_template($located);
+
+		return $located;
+	}
+}
+
 if ( empty( $_GET['object_ids'] ) ) {
 	GeoMashupQuery::generate_location_json( );
 } else {
