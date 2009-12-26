4c478cd386bd3e5569d37a406446d5510d0fb630
dylankkuhn <dylankkuhn@81465218-ea22-0410-8f2b-2343bde7f0e1>
2009-12-26 00:47:12 +0000

    Added an uninstall procedure.
    Update issue 289
    Status: Implemented
    I hope WordPress gives you enough warning, because now you can delete all your Geo Mashup data...
    
    
    git-svn-id: https://wordpress-geo-mashup.googlecode.com/svn/trunk/geo-mashup@571 81465218-ea22-0410-8f2b-2343bde7f0e1

---
+++ uninstall.php
@@ -0,0 +1,32 @@
+<?php
+/** 
+ * Geo Mashup uninstall procedure.
+ *
+ * @package GeoMashup
+ */
+
+// Make sure this is a legitimate uninstall request
+if( ! defined( 'ABSPATH') or ! defined('WP_UNINSTALL_PLUGIN') or ! current_user_can( 'delete_plugins' ) )
+	exit();
+
+function geo_mashup_uninstall_db() {
+	global $wpdb;
+	$tables = array( 'geo_mashup_administrative_names', 'geo_mashup_location_relationships', 'geo_mashup_locations' );
+	foreach( $tables as $table ) {
+		$wpdb->query( 'DROP TABLE IF EXISTS ' . $wpdb->prefix . $table );
+	}
+}
+
+function geo_mashup_uninstall_options() {
+	delete_option( 'geo_mashup_temp_kml_url' );
+	delete_option( 'geo_mashup_db_version' );
+	delete_option( 'geo_mashup_activation_log' );
+	delete_option( 'geo_mashup_options' );
+	delete_option( 'geo_locations' );
+	// Leave the google_api_key option 
+	// Still belongs to this site, and may be used by other plugins
+}
+
+geo_mashup_uninstall_db();
+geo_mashup_uninstall_options();
+?>
