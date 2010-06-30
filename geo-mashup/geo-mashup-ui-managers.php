d61c13ea937b6d3d06c122d6882631968e503f9c
dylankkuhn <dylankkuhn@81465218-ea22-0410-8f2b-2343bde7f0e1>
2010-06-30 19:10:04 +0000

    Add the location editor meta box to all UI-enabled post types. Fixes issue 362
    git-svn-id: https://wordpress-geo-mashup.googlecode.com/svn/trunk@716 81465218-ea22-0410-8f2b-2343bde7f0e1

--- geo-mashup/geo-mashup-ui-managers.php
+++ geo-mashup/geo-mashup-ui-managers.php
@@ -377,9 +377,8 @@ class GeoMashupPostUIManager extends GeoMashupUIManager {
 		add_filter( 'upload_mimes', array( &$this, 'upload_mimes' ) );
 
 		// Enable this interface when the option is set 
-		$enabled = $geo_mashup_options->get( 'overall', 'located_object_name', 'post' ) == 'true';
 
-		if ( $enabled ) { 
+		if ( 'true' == $geo_mashup_options->get( 'overall', 'located_object_name', 'post' ) ) {
 
 			// Queue inline location handlers
 
@@ -418,7 +417,7 @@ class GeoMashupPostUIManager extends GeoMashupUIManager {
 	}
 
 	/**
-	 * Add a location meta box to the post and page editors.
+	 * Add a location meta box to the post editors.
 	 * 
 	 * admin_menu {@link http://codex.wordpress.org/Plugin_API/Action_Reference action}
 	 * called by Wordpress.
@@ -428,8 +427,17 @@ class GeoMashupPostUIManager extends GeoMashupUIManager {
 	 */
 	function admin_menu() {
 		// Not adding a menu, but at this stage add_meta_box is defined, so we can add the location form
-		add_meta_box( 'geo_mashup_post_edit', __( 'Location', 'GeoMashup' ), array( &$this, 'print_form' ), 'post', 'advanced' );
-		add_meta_box( 'geo_mashup_post_edit', __( 'Location', 'GeoMashup' ), array( &$this, 'print_form' ), 'page', 'advanced' );
+		if ( function_exists( 'get_post_types' ) ) {
+			$post_types = get_post_types( array(), 'objects' );
+			foreach ( $post_types as $post_type ) {
+				if ( $post_type->show_ui ) {
+					add_meta_box( 'geo_mashup_post_edit', __( 'Location', 'GeoMashup' ), array( &$this, 'print_form' ), $post_type->name, 'advanced' );
+				}
+			}
+		} else {
+			add_meta_box( 'geo_mashup_post_edit', __( 'Location', 'GeoMashup' ), array( &$this, 'print_form' ), 'post', 'advanced' );
+			add_meta_box( 'geo_mashup_post_edit', __( 'Location', 'GeoMashup' ), array( &$this, 'print_form' ), 'page', 'advanced' );
+		}
 	}
 
 	/**
