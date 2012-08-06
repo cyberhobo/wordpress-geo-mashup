f3def7d9b12e91a6552b2866b0735aefa09e3442
dylankkuhn <dylankkuhn@81465218-ea22-0410-8f2b-2343bde7f0e1>
2012-08-06 00:27:43 +0000

    Incorporate existing querystring parameters if frame URLs.
    Update Issue 536
    Status: Fixed
    I don't have a test installation for this, but it doesn't break 
    my usual tests, so I may release it so you can give it a try.
    
    
    git-svn-id: https://wordpress-geo-mashup.googlecode.com/svn/trunk@1123 81465218-ea22-0410-8f2b-2343bde7f0e1

--- geo-mashup/geo-mashup.php
+++ geo-mashup/geo-mashup.php
@@ -812,6 +812,37 @@ class GeoMashup {
 	}
 
 	/**
+	 * Make an URL with querystring added to the site's home URL.
+	 *
+	 * @since 1.4
+	 *
+	 * @static
+	 * @param array $query Associative array of querystring parameters.
+	 * @return string HTML-ready URL.
+	 */
+	public static function build_home_url( $query = array() ) {
+
+		// Sending no path to home_url() provides compatbility with WPML, which may change the domain this way
+		$home_url_parts = parse_url( home_url() );
+
+		// Language plugins may also add query parameters to home_url(). We'll add to these.
+		$home_url_query_parts = array();
+		if ( !empty( $home_url_parts['query'] ) ) {
+			$home_url_query_parts = array();
+			wp_parse_str( $home_url_parts['query'], $home_url_query_parts );
+			$query = array_merge( $home_url_query_parts, $query );
+		}
+
+		$home_url = $home_url_parts['scheme'] . '://' . $home_url_parts['host'];
+		if ( !empty( $home_url_parts['path'] ) )
+			$home_url .= $home_url_parts['path'];
+		if ( !empty( $query ) )
+			$home_url .=  '?' . htmlspecialchars( http_build_query( $query ) );
+
+		return $home_url;
+	}
+
+	/**
 	 * The map template tag.
 	 *
 	 * Returns HTML for a Google map. Must use with echo in a template: echo GeoMashup::map();.
@@ -992,12 +1023,16 @@ class GeoMashup {
 		set_transient( 'gmm' . $atts_md5, $map_data, 20 );
 		set_transient( 'gmp' . $atts_md5, $atts, 60*60*24 );
 
-		// Sending no path to home_url() provides compatbility with WPML, which may change the domain this way
-		$iframe_src =  path_join( home_url(), '?geo_mashup_content=render-map&amp;map_data_key=' . $atts_md5 );
+		$src_args = array(
+			'geo_mashup_content' => 'render-map',
+			'map_data_key' => $atts_md5,
+		);
 
 		if ( !empty( $atts['lang'] ) )
-			$iframe_src .= '&amp;lang=' . $atts['lang'];
-			
+			$src_args['lang'] = $atts['lang'];
+
+		$iframe_src = self::build_home_url( $src_args );
+
 		$content = "";
 
 		if ($click_to_load == 'true') {
