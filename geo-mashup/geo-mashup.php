7384fd4d96a98ff9b1e4b8007b395a98b1a0bb1f
dylankkuhn <dylankkuhn@81465218-ea22-0410-8f2b-2343bde7f0e1>
2012-11-04 16:27:46 +0000

    Bump version for 1.5 beta 1 (1.4.99.0).
    git-svn-id: https://wordpress-geo-mashup.googlecode.com/svn/branches/1.5-dev@1149 81465218-ea22-0410-8f2b-2343bde7f0e1

--- geo-mashup/geo-mashup.php
+++ geo-mashup/geo-mashup.php
@@ -3,7 +3,7 @@
 Plugin Name: Geo Mashup
 Plugin URI: http://code.google.com/p/wordpress-geo-mashup/ 
 Description: Save location for posts and pages, or even users and comments. Display these locations on Google maps. Make WordPress into your GeoCMS.
-Version: 1.4.98.0
+Version: 1.4.99.0
 Author: Dylan Kuhn
 Author URI: http://www.cyberhobo.net/
 Minimum WordPress Version Required: 3.0
@@ -196,7 +196,7 @@ class GeoMashup {
 		define('GEO_MASHUP_URL_PATH', trim( plugin_dir_url( __FILE__ ), '/' ) );
 		define('GEO_MASHUP_MAX_ZOOM', 20);
 		// Make numeric versions: -.02 for alpha, -.01 for beta
-		define('GEO_MASHUP_VERSION', '1.4.98.0');
+		define('GEO_MASHUP_VERSION', '1.4.99.0');
 		define('GEO_MASHUP_DB_VERSION', '1.3');
 	}
 
