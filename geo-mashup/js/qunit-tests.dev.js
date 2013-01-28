95336f4ece496bf08c925141721e4b0a3324f212
dylankkuhn <dylankkuhn@81465218-ea22-0410-8f2b-2343bde7f0e1>
2013-01-28 20:50:29 +0000

    Test initial zoom and center on first marker.
    git-svn-id: https://wordpress-geo-mashup.googlecode.com/svn/branches/1.5-dev@1213 81465218-ea22-0410-8f2b-2343bde7f0e1

--- geo-mashup/js/qunit-tests.dev.js
+++ geo-mashup/js/qunit-tests.dev.js
@@ -9,65 +9,62 @@ jQuery( function( $ ) {
 			.attr( 'name', gm_test_data.name )
 			.attr( 'width', gm_test_data.width )
 			.attr( 'height', gm_test_data.height )
+			.appendTo( '#qunit-fixture' )
 			.attr( 'src' , url );
 
-		return $frame.appendTo( '#qunit-fixture' ).load( callback );
+		return $frame.load( callback );
 	}
 
 	$.each( gm_test_data.test_apis, function( i, api ) {
 
-		asyncTest( api + " global loads", 8, function() {
-			var $frame = loadTestFrame( gm_test_data.global_urls[api], function() {
+		asyncTest( api + " global loads", 9, function() {
+			loadTestFrame( gm_test_data.global_urls[api], function() {
 				var gm = window.frames[gm_test_data.name].GeoMashup;
 				ok( gm, 'GeoMashup object is available' );
 				ok( gm.map, 'map object is available' );
-				gm.map.load.addHandler( function() {
-					ok( gm.map.markers, 'markers are available' );
-					equal( gm.map.markers.length, location_count, 'a marker is created for each location' );
-					ok( gm.map.polylines, 'polylines are available' );
-					equal( gm.map.polylines.length, 1, 'a polyline is created for each term with line zoom set' );
-					QUnit.close(
-						gm.map.getCenter().lat,
-						37.8,
-						0.5,
-						'map center latitude is near the middle of locations'
-					);
-					QUnit.close(
-						gm.map.getCenter().lon,
-						-116.7,
-						0.5,
-						'map center longitude is near the middle of locations'
-					);
-					start();
-				} );
+				ok( gm.map.markers, 'markers are available' );
+				equal( gm.map.markers.length, location_count, 'a marker is created for each location' );
+				ok( gm.map.polylines, 'polylines are available' );
+				equal( gm.map.polylines.length, 1, 'a polyline is created for each term with line zoom set' );
+				equal( gm.map.getZoom(), 10, 'initial zoom is as specified (10)' );
+				QUnit.close(
+					gm.map.getCenter().lat,
+					gm.map.markers[0].location.lat,
+					0.005,
+					'map center latitude is near the first marker'
+				);
+				QUnit.close(
+					gm.map.getCenter().lon,
+					gm.map.markers[0].location.lon,
+					0.005,
+					'map center longitude is near the first marker'
+				);
+				start();
 			} );
 		} );
 
 		asyncTest( api + " markers respond", location_count*3, function() {
-			var $frame = loadTestFrame( gm_test_data.global_urls[api], function() {
+			loadTestFrame( gm_test_data.global_urls[api], function() {
 				var gm = window.frames[gm_test_data.name].GeoMashup;
-				gm.map.load.addHandler( function() {
-					$.each( gm.map.markers, function( i, marker ) {
-						marker.click.fire();
-						equal( gm.selected_marker, marker, 'a clicked marker is selected' );
-						QUnit.close(
-							marker.location.lat,
-							gm.map.getCenter().lat,
-							0.005,
-							'map center latitude is close to clicked marker'
-						);
-						QUnit.close(
-							marker.location.lon,
-							gm.map.getCenter().lon,
-							0.005,
-							'map center longitude is close to clicked marker'
-						);
-					} );
-					start();
+				$.each( gm.map.markers, function( i, marker ) {
+					// Firefox breaking very strangely here, why?
+					marker.click.fire();
+					equal( gm.selected_marker, marker, 'a marker is selected when clicked' );
+					QUnit.close(
+						marker.location.lat,
+						gm.map.getCenter().lat,
+						0.005,
+						'map center latitude is close to selected marker'
+					);
+					QUnit.close(
+						marker.location.lon,
+						gm.map.getCenter().lon,
+						0.005,
+						'map center longitude is close to selected marker'
+					);
 				} );
+				start();
 			} );
 		} );
 	} );
-
-
 } );
