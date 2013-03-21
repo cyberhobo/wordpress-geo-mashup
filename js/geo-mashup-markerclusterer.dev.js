5cdf842e1d531c2a29a810a6d5a818d00db35a39
Dylan Kuhn <dylan.k.kuhn@gmail.com>
2013-03-20 19:18:26 -0700

    Add an action to modify MarkerClustererOptions.

--- js/geo-mashup-markerclusterer.dev.js
+++ js/geo-mashup-markerclusterer.dev.js
@@ -10,20 +10,28 @@
  * @param markers
  */
 GeoMashup.addMarkers = function( markers ) {
-	var proprietary_markers = [];
+	var proprietary_markers = [],
+		options = {
+			ignoreHidden: true,
+			minimumClusterSize: 4,
+			maxZoom: parseInt( this.opts.cluster_max_zoom ),
+			zoomOnClick: true
+		};
 	this.forEach( markers, function( i, marker ) {
 		this.map.addMarker( marker );
 		proprietary_markers.push( marker.proprietary_marker );
 	} );
-	this.clusterer = new MarkerClusterer( this.map.getMap(), proprietary_markers, {
-		ignoreHidden: true,
-		minimumClusterSize: 4,
-		maxZoom: parseInt( this.opts.cluster_max_zoom ),
-		zoomOnClick: true
-	} );
+	/**
+	 * The MarkerClustererPlus object is being created. Use this to change the options.
+	 * @name GeoMashup#markerClustererOptions
+	 * @event
+	 * @param {Object} options Modifiable <a href="http://google-maps-utility-library-v3.googlecode.com/svn/trunk/markerclustererplus/docs/reference.html#MarkerClustererOptions">MarkerClustererOptions</a>.
+	 */
+	this.doAction( 'markerClustererOptions', options );
+	this.clusterer = new MarkerClusterer( this.map.getMap(), proprietary_markers, options );
 };
 
 GeoMashup.recluster = function() {
 	this.clusterer.repaint();
-}
+};
 
