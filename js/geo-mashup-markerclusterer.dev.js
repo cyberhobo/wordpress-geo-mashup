4876cd27ad83e1b1499b077e4d298b2971a77a28
Dylan Kuhn <dylan.k.kuhn@gmail.com>
2013-03-24 08:41:17 -0700

    Use a local cluster icon.
    Update Issue 381
    Status: Implemented
    Labels: Milestone-1.6
    Got MarkerClusterer working! There has been lots of interest in this but
    no financial support - donations would help to test, polish, and release it.
    
    https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=11045324

--- js/geo-mashup-markerclusterer.dev.js
+++ js/geo-mashup-markerclusterer.dev.js
@@ -15,12 +15,39 @@ GeoMashup.addMarkers = function( markers ) {
 			ignoreHidden: true,
 			minimumClusterSize: 4,
 			maxZoom: parseInt( this.opts.cluster_max_zoom ),
-			zoomOnClick: true
+			zoomOnClick: true,
+			styles: [
+				{
+					fontFamily: 'Ubuntu Condensed,Trebuchet MS,Verdana,sans-serif',
+					textColor: 'white',
+					height: 45,
+					width: 45,
+					url: this.opts.url_path + '/images/cluster.png'
+				}
+
+			]
 		};
 	this.forEach( markers, function( i, marker ) {
 		this.map.addMarker( marker );
 		proprietary_markers.push( marker.proprietary_marker );
 	} );
+
+	options.styles.push( this.clone( options.styles[0]) );
+	options.styles[1].width = 52;
+	options.styles[1].height = 52;
+	options.styles[1].textSize = 12;
+	options.styles.push( this.clone( options.styles[0]) );
+	options.styles[2].width = 63;
+	options.styles[2].height = 63;
+	options.styles[2].textSize = 13;
+	options.styles.push( this.clone( options.styles[0]) );
+	options.styles[3].width = 75;
+	options.styles[3].height = 75;
+	options.styles[3].textSize = 14;
+	options.styles.push( this.clone( options.styles[0]) );
+	options.styles[4].width = 90;
+	options.styles[4].height = 90;
+	options.styles[4].textSize = 15;
 	/**
 	 * The MarkerClustererPlus object is being created. Use this to change the options.
 	 * @name GeoMashup#markerClustererOptions
